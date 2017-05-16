8cb9a5016c5e168889eded952294e45c9fa6c74c
x404 <kronos2003@gmail.com>
2017-05-16 13:14:58 +0300

    Добавлено мобильное меню

--- js/engine.js
+++ js/engine.js
@@ -10,26 +10,57 @@ $(document).ready(function(){
 	});
 
 
-		// прячем кнопку #back-top
-		$("#to-top").hide();
-	
-		$(window).scroll(function (){
-			if ($(this).scrollTop() > 200){
-				$("#to-top").fadeIn();
-			} else{
-				$("#to-top").fadeOut();
-			}
-		});
+	// прячем кнопку #back-top
+	$("#to-top").hide();
 
-		// при клике на ссылку плавно поднимаемся вверх
-		$("#to-top").click(function (){
-			$("body,html").animate({
-				scrollTop:0
-			}, 800);
-			return false;
-		});
+	$(window).scroll(function (){
+		if ($(this).scrollTop() > 200){
+			$("#to-top").fadeIn();
+		} else{
+			$("#to-top").fadeOut();
+		}
+	});
+
+	// при клике на ссылку плавно поднимаемся вверх
+	$("#to-top").click(function (){
+		$("body,html").animate({
+			scrollTop:0
+		}, 800);
+		return false;
+	});
 
 
+	$(window).resize(function(){
+		if ($('body').width() > 640) {
+			$('body').removeClass('o-menu');
+			$('#navbar').css('height', 'auto');
+		}
+	});
+
+	// mobile-menu
+	$('#navbar').each(function(){
+		var $this = $(this),
+			$link = $('.navbar-toggle'),
+			$close = $('.close-menu'),
+
+			init = function(){
+				$link.on('click', openMenu);
+				$close.on('click', closeMenu);
+			},
+			openMenu = function(e){
+				e.preventDefault();
+				h = $(document).height();
+				$('body').addClass('o-menu');
+				$('#navbar').height(h);
+
+			},
+			closeMenu = function(e){
+				e.preventDefault();
+				$('body').removeClass('o-menu');
+				$('#navbar').height('auto');
+			};
+		init();
+	});	
 });
 
 // =заглушка для IE
